86a068e28de7a7e9122d9ee24a929568fafff142
andylamax <andylamax@programmer.net>
2020-04-12 13:29:54 +0300

    releasing 1.0.0-alpha1

--- src/commonMain/kotlin/tz/co/asoft/sendgrid/SendGridEmailGateway.kt
+++ src/commonMain/kotlin/tz/co/asoft/sendgrid/SendGridEmailGateway.kt
@@ -5,17 +5,21 @@ import io.ktor.client.request.header
 import io.ktor.client.request.post
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
-import tz.co.asoft.comms.EmailMessagingService as EmailService
+import tz.co.asoft.email.Email
+import tz.co.asoft.email.entities.EmailMessage
+import tz.co.asoft.email.gateway.IEmailGateway
+import tz.co.asoft.io.File
+import tz.co.asoft.persist.repo.IRepo
 
-class EmailMessagingService(private val apiKey: String) : EmailService {
+class SendGridEmailGateway(private val apiKey: String, override val repo: IRepo<EmailMessage>) : IEmailGateway {
     private val client = HttpClient { }
     private val url = "https://api.sendgrid.com/v3/mail/send"
-    private fun content(sender: String, receivers: List<String>, subject: String?, body: String, type: String): String {
+    private fun content(sender: String, receivers: List<Email>, subject: String?, body: String, type: String): String {
         val cntnt = """
         {
           "personalizations": [
             {
-              "to": [${receivers.joinToString(",") { """{ "email":"$it"}""" }}]
+              "to": [${receivers.joinToString(",") { """{ "email":"${it.value}"}""" }}]
             }
           ],
           "from": {
@@ -33,12 +37,13 @@ class EmailMessagingService(private val apiKey: String) : EmailService {
         return cntnt
     }
 
-    override suspend fun send(sender: String, receivers: List<String>, subject: String?, body: String, hasHtmlContent: Boolean) {
+    override suspend fun send(sender: String, receivers: List<Email>, subject: String?, body: String, attachments: List<File>): EmailMessage {
         client.post<String>(url) {
             header("Authorization", "Bearer $apiKey")
             contentType(ContentType.Application.Json)
-            val type = if (hasHtmlContent) "text/html" else "text/plain"
-            this.body = content(sender, receivers, subject, body, type)
+            this.body = content(sender, receivers, subject, body, "text/html")
         }
+        val email = EmailMessage(sender, receivers.map { it.value }, subject, body)
+        return repo.create(email)
     }
 }
